8e8ae6ed7625e8c2458f489cb18891359a6da3a8
chime96 <40341041+chime96@users.noreply.github.com>
2021-01-18 13:34:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+
+
+/* Sigup confirm password */
+
+var password = document.getElementById("password"),
+  confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
